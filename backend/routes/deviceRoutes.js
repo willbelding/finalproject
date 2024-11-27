666aadee3ecebe972b3e6fd2c94ab15eeaf36d49
willbelding <114653574+willbelding@users.noreply.github.com>
2024-11-27 13:37:53 +0000

    Update deviceRoutes.js
    Updated route methods

--- backend/routes/deviceRoutes.js
+++ backend/routes/deviceRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
 const deviceController = require('../controllers/deviceController');
 
-router.get('/devices', deviceController.getDevices);
+router.get('/devices', deviceController.getAllDevices);
+
+router.get('/devices/:id', deviceController.getDeviceById);
+
 router.post('/devices', deviceController.addDevice);
+
+router.put('/devices/:id', deviceController.updateDevice);
+
+router.delete('/devices/:id', deviceController.deleteDevice);
 
 module.exports = router;
